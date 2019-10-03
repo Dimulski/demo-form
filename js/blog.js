0e8730ad4815bf3cd9912eaaa619652dc83ca06a
Dimulski <george.dimulski@gmail.com>
2019-10-03 17:15:44 +0300

    First commit

---
+++ js/blog.js
@@ -0,0 +1,55 @@
+getPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return await response.json();
+};
+
+getPosts().then(posts => {
+    const blogField = document.getElementById("blog-field");
+    posts.filter(posts => posts.id > 10 && posts.id < 23).forEach(post => {
+        const blogCard = document.createElement("div");
+        blogCard.className = "card";
+        blogCard.innerHTML =
+        `<div class="card-body">
+            <h5 class="card-title">${post.title.charAt(0).toUpperCase() + post.title.slice(1)}</h5>
+            <p class="card-text">${post.body.length > 59 ?
+                post.body.charAt(0).toUpperCase() + post.body.substring(1,59) + "..." :
+                post.body.charAt(0).toUpperCase() + post.body}</p>
+            <a href="#" class="btn btn-primary read-more"  data-id="${post.id}">Read More</a>
+        </div>`;
+        blogField.appendChild(blogCard);
+    });
+
+    [].forEach.call(document.getElementsByClassName("read-more"), (button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            const id = button.dataset.id;
+            window.location = 'post.html?id=' + id;
+        });
+    });
+});
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(response => response.json())
+// .then(json => {
+//     const blogField = document.getElementById("blog-field");
+//     json.filter(post => post.id > 10 && post.id < 23).forEach(post => {
+//         const blogCard = document.createElement("div");
+//         blogCard.className = "card";
+//         blogCard.innerHTML =
+//         `<div class="card-body">
+//             <h5 class="card-title">${post.title.charAt(0).toUpperCase() + post.title.slice(1)}</h5>
+//             <p class="card-text">${post.body.charAt(0).toUpperCase() + post.body.substring(1,60)}...</p>
+//             <a href="#" class="btn btn-primary read-more"  data-id="${post.id}">Read More</a>
+//         </div>`;
+//         blogField.appendChild(blogCard);
+//     });
+
+//     [].forEach.call(document.getElementsByClassName("read-more"), (button) => {
+//         button.addEventListener("click", (event) => {
+//             event.preventDefault();
+//             const id = button.dataset.id;
+//             window.location = 'post.html?id=' + id;
+//         })
+//     })
+// });
---
+++ js/photos.js
@@ -0,0 +1,83 @@
+get12random = (elements) => {
+    let selected = [];
+    while (selected.length < 12) {
+        let randomElement = Math.floor(Math.random() * elements.length + 1);
+        if (selected.indexOf(randomElement) === -1) {
+            selected.push(randomElement);
+        }
+    }
+
+    return selected;
+};
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const url = entry.target.children[0].dataset.url;
+                entry.target.children[0].src = url;
+                
+                entry.target.children[0].classList.remove("hidden");
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
+};
+
+const photosField = document.getElementById("photos-field");
+get12random([...Array(50).keys()]).forEach(pictureId => {
+    axios.get(`https://jsonplaceholder.typicode.com/photos/${pictureId}`)
+    .then(response => {
+        const photo = response.data;
+        const photoCard = document.createElement("div");
+            photoCard.className = "photo-card";
+            photoCard.innerHTML =
+                `<img src="../img/600.png" data-url='${photo.url}' class="photo-img-top hidden" alt="${photo.title}">
+                <div class="photo-body">
+                    <h5 class="photo-title">${photo.title}</h5>
+                </div>`;
+            photosField.appendChild(photoCard);
+            lazyLoad(photoCard);
+    });
+});
+
+
+// get12random = (elements) => {
+//     var selected = [];
+//     while (selected.length < 12) {
+//         let randomElement = elements[Math.floor(Math.random() * elements.length)];
+//         if (selected.indexOf(randomElement) === -1) {
+//             selected.push(randomElement);
+//         }
+//     }
+
+//     return selected;
+// };
+
+// axios.get('https://jsonplaceholder.typicode.com/photos')
+//     .then(response => {
+//         const photosField = document.getElementById("photos-field");
+//         const first50photos = response.data.filter(photo => photo.id < 51);
+//         const random12 = get12random(first50photos);
+
+//         random12.forEach(photo => {
+//             const photoCard = document.createElement("div");
+//             photoCard.className = "photo-card";
+//             photoCard.innerHTML =
+//                 `<img src="${photo.url}" class="photo-img-top d-none" alt="${photo.title}">
+//                 <div class="photo-body d-none">
+//                     <h5 class="photo-title">${photo.title}</h5>
+//                 </div>`;
+//             photosField.appendChild(photoCard);
+//             lazyLoad(photoCard);
+//         });
+//     });
+
+
+// const observerOptions = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: []
+// };
